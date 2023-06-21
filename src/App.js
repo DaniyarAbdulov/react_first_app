16b900ed3b9675b39f8973fe7880a581acf359b4
daniyarabdulov <daniyar0303@gmail.com>
2023-06-22 00:02:29 +0600

    third commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Header from './Header'; 
-import Technologies from './Technologies';
-import Footer from './Footer';
-
 
 
 const App = () => {
   return (
-     <div>
-        <Header />
-        <Technologies />
-        <Footer/>
+     <div className='app-wrapper'>
+        <header className='header'>
+          <img src= 'https://jusan.kz/file-server/filename?dir=logo&filename=logo-desktop2.webp' />
+          
+        </header>
+      <nav className='nav'> 
+        SideBar 
+      </nav>
+      <div className='content'> 
+          Main Conten
+        </div> 
      </div>
   
   );
