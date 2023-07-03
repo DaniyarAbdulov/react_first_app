0b7392f5fcf9ab0213949db3e5384f54ed47bf52
HQ Jusan Bank <hqjusanbank@Mac-mini-HQ-3.local>
2023-07-03 17:04:45 +0600

    10th commit

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,12 +1,12 @@
-import React from "react";
 import classes from "./Dialogs.module.css";
 import { NavLink } from "react-router-dom";
+import React from "react";
 
 const DialogItem = (props) => {
   let path = "/dialogs/" + props.id;
   return (
     <div className={classes.dialog + " " + classes.active}>
-      <NavLink to={path}>{props.name}</NavLink>
+      <NavLink to={path}className={(navData)=> navData.isActive ? classes.active : classes.dialog}>{props.name} </NavLink>
     </div>
   );
 };
@@ -16,18 +16,27 @@ const Message = (props) => {
 };
 
 const Dialogs = (props) => {
+  let dialogs = [
+    { id: 1, name: "Michael" },
+    { id: 2, name: "Trevor" },
+    { id: 3, name: "Lester" },
+  ];
+
+  let messages = [
+    { id: 1, message: "Hello Kid!" },
+    { id: 2, message: "Where is Lamar?" },
+    { id: 3, message: "Did you do it?" },
+  ];
+
+  let dialogsElements = dialogs.map((d) => (
+    <DialogItem name={d.name} id={d.id} />
+  ));
+  let messagesElements = messages.map((m) => <Message message={m.message} />);
+
   return (
     <div className={classes.dialogs}>
-      <div className={classes.dialogsItems}>
-        <DialogItem name="Michael" id="1" />
-        <DialogItem name="Trevor" id="2" />
-        <DialogItem name="Lester" id="3" />
-      </div>
-      <div className={classes.messages}>
-        <Message message="Hello Kid!" />
-        <Message message="Where am I?" />
-        <Message message="Did you do it?" />
-      </div>
+      <div className={classes.dialogsItems}>{dialogsElements}</div>
+      <div className={classes.messages}>{messagesElements}</div>
     </div>
   );
 };
--- src/components/Profile/My Posts/MyPosts.jsx
+++ src/components/Profile/My Posts/MyPosts.jsx
@@ -5,6 +5,17 @@ import AddPostField from "./TextField_MU";
 import AddPostButton from "./Button_TextField_MU";
 
 const MyPosts = () => {
+  let posts = [
+    { id: 1, message: "Hello world" },
+    { id: 2, message: "My first app" },
+    { id: 3, message: "Where's my God?" },
+    {
+      id: 4,
+      message: "Once you're in the Hell. Only the Devil can help you out}",
+    },
+  ];
+  let postsElemets = posts.map((p) => <Post message={p.message} />);
+
   return (
     <div className={classes.postsBlock}>
       <h3>My posts</h3>
@@ -12,13 +23,7 @@ const MyPosts = () => {
         <AddPostField />
         <AddPostButton />
       </div>
-      <div className={classes.posts}>
-        <Post message="Hello world" />
-        <Post message="Hello world" />
-        <Post message="My first app" />
-        <Post message="Where's my God?" />
-        <Post message="Once you're in the Hell. Only the Devil can help you out" />
-      </div>
+      <div className={classes.posts}>{postsElemets}</div>
     </div>
   );
 };
