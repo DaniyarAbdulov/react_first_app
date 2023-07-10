571fa845eae30820e68b2cccd895052e778add1e
HQ Jusan Bank <hqjusanbank@Mac-mini-HQ-3.local>
2023-07-10 17:19:26 +0600

    14th commit

--- src/components/Profile/My Posts/MyPosts.jsx
+++ src/components/Profile/My Posts/MyPosts.jsx
@@ -1,19 +1,30 @@
 import React from "react";
 import classes from "../My Posts/MyPosts.module.css";
 import Post from "./Post/Post";
-import AddPostField from "./TextField_MU";
-import AddPostButton from "./Button_TextField_MU";
 
 const MyPosts = (props) => {
-  
   let postsElemets = props.posts.map((p) => <Post message={p.message} />);
+  let newPostElement = React.createRef();
+  let addPost = () => {
+    let text = newPostElement.current.value;
+    alert(text);
+  };
 
   return (
     <div className={classes.postsBlock}>
       <h3>My posts</h3>
       <div>
-        <AddPostField />
-        <AddPostButton />
+        <div>
+          <textarea
+            ref={newPostElement}
+            className={classes.textField}
+          ></textarea>
+        </div>
+        <div>
+          <button className={classes.button} onClick={addPost}>
+            Add post
+          </button>
+        </div>
       </div>
       <div className={classes.posts}>{postsElemets}</div>
     </div>
